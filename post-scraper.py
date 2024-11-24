c6feba55ed81e6add67c9386742b27f88f008604
Nintails-TF <2455717@dundee.ac.uk>
2024-11-24 12:00:28 +0000

    Created checklist

---
+++ post-scraper.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+"""
+- Get a list of URLs, that I'd want to look at.
+    - Having a single website would work for now.
+- Figure out the querying process of each main jobsite.
+- Send a GET request to the website.
+- Extract and parse meaningful data from any job postings.
+- Go to the next page
+- Be careful not to spam the website with too many requests at once.
+"""
+
